71ccd421c174c0938b1c1727495e9ddf2db2115f
Mariacruz4 <eloisa943@gmail.com>
2019-07-16 16:03:41 -0500

    first commit

---
+++ U1-M3-Summative-Cruz-Maria/src/main/java/com/ecompany/U1M3SummativeCruzMaria/U1M3SummativeCruzMariaApplication.java
@@ -0,0 +1,13 @@
+package com.ecompany.U1M3SummativeCruzMaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M3SummativeCruzMariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M3SummativeCruzMariaApplication.class, args);
+	}
+
+}
---
+++ U1-M3-Summative-Cruz-Maria/src/test/java/com/ecompany/U1M3SummativeCruzMaria/U1M3SummativeCruzMariaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ecompany.U1M3SummativeCruzMaria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class U1M3SummativeCruzMariaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
